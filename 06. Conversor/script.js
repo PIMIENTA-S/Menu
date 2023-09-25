20d756fc5f33f7900baab9f7e9db570e2c5ac626
Angell Pimienta's <80433456+PIMIENTA-S@users.noreply.github.com>
2023-09-24 19:48:11 -0500

    Change conversor

--- 06. Conversor/script.js
+++ 06. Conversor/script.js
@@ -1,1 +1,25 @@
-let valor = document.getElementById('valor')
+const valor = document.getElementById('valor');
+const resultado = document.getElementById('resultado');
+const unidad1 = document.getElementById('unidad1');
+const unidad2 = document.getElementById('unidad2');
+
+valor.addEventListener('keyup', convertir);
+unidad1.addEventListener('change', convertir);
+unidad2.addEventListener('change', convertir);
+
+let final;
+
+function convertir (){
+    if (valor.value == ""){
+        return ""
+    }
+    number = valor.value;
+    number = parseFloat(number);
+
+    if (unidad1.value == "Kilometros" && unidad2.value == "Kilometros"){
+        final = number * 1
+        resultado.innerHTML = number + " Kilometros equivalen a " + final + " Kilometros"
+    }
+}
+
+
